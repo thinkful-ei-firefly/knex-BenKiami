e86087fdc528ba858620e5731e0385ea949f1782
Kiami Nichols <kiamimariah@yahoo.com>
2019-08-27 14:43:19 -0400

    gitignore, js

---
+++ src/shopping-list-service.js
@@ -0,0 +1,30 @@
+const shoppingListService = {
+    getAllItems(knex) {
+        return knex
+        .select('*')
+        .from('shopping_list')
+    },
+
+    getById(knex, id) {
+        return knex.from('shopping_list')
+        .select('*')
+        .where('id', id)
+        .first()
+    },
+    
+    deleteItem(knex, id) {
+        return knex('shopping_list')
+        .where({ id })
+        .update(newItemFields)
+    },
+
+    insertItem(knex, newItem) {
+        return knex
+        .insert(newItem)
+        .into('shopping_list')
+        .returning('*')
+        .then(rows => rows[0])
+    },
+}
+
+module exports = ShoppingListService
